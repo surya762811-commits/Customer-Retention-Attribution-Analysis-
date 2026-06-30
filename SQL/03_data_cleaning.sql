a7f035b090a7cd7c4393463c065cd199b1375c3d
Varun Kumar <surya762811@gmail.com>
2026-06-30 13:21:54 +0530

    Update 03_data_cleaning.sql

--- SQL/03_data_cleaning.sql
+++ SQL/03_data_cleaning.sql
@@ -1,1 +1,9 @@
+-- --- -- Check Null Customer -- --- --
+SELECT COUNT(*)
+FROM transactions
+WHERE customer_id IS NULL;
 
+-- --- -- Check Invalid Sales -- --- -- 
+SELECT COUNT(*)
+FROM transactions
+WHERE sales <= 0;
