18ed03db19fee2aab7580a09a1869d7c66ba85ed
Varun Kumar <surya762811@gmail.com>
2026-06-30 13:20:46 +0530

    Update 02_create_tables.sql

--- SQL/02_create_tables.sql
+++ SQL/02_create_tables.sql
@@ -7,3 +7,12 @@ quantity INT,
 sales DECIMAL(10,2),
 country VARCHAR(100)
 );
+
+
+CREATE TABLE marketing_touchpoints (
+touch_id INT,
+customer_id INT,
+channel VARCHAR(50),
+campaign VARCHAR(100),
+touch_date DATETIME
+);
