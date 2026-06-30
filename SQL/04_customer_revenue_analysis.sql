246cad65261e9f35f0a0cb59117b3cdc2ba10732
Varun Kumar <surya762811@gmail.com>
2026-06-30 13:22:35 +0530

    Update 04_customer_revenue_analysis.sql

--- SQL/04_customer_revenue_analysis.sql
+++ SQL/04_customer_revenue_analysis.sql
@@ -1,1 +1,12 @@
+WITH customer_revenue AS
+(
+SELECT
+customer_id,
+SUM(sales) total_revenue
+FROM transactions
+GROUP BY customer_id
+)
 
+SELECT *
+FROM customer_revenue
+ORDER BY total_revenue DESC;
