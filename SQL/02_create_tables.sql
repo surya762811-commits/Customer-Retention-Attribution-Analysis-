f6c42540a07a324b589f6e2ed5d4f065537b3a99
Varun Kumar <surya762811@gmail.com>
2026-06-30 13:19:54 +0530

    Update 02_create_tables.sql

--- SQL/02_create_tables.sql
+++ SQL/02_create_tables.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE transactions (
+transaction_id INT,
+customer_id INT,
+order_date DATETIME,
+product_category VARCHAR(100),
+quantity INT,
+sales DECIMAL(10,2),
+country VARCHAR(100)
+);
